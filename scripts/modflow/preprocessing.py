6e04d90624e13e20ed1c18974a24219ad7796f9d
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2020-05-13 14:55:33 +0200

    script to get REGIS as netcdf for extent

---
+++ scripts/modflow/preprocessing.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import xarray as xr
+
+sys.path.insert(1, "../mfutil")
+import mgrid
+
+# %% extent
+extent = (112000.0, 115200.0, 444800.0, 447000.0)
+
+# %% regis
+cs_regis = 100.  # cell size regis
+url = r'http://www.dinodata.nl:80/opendap/REGIS/REGIS.nc'
+
+# redefine extent, nrow & ncol (fit to regis)
+extent, nrow, ncol = mgrid.fit_extent_to_regis(extent, cs_regis, cs_regis)
+
+# get number of layers
+regis_ds_raw = xr.open_dataset(url).sel(x=slice(extent[0], extent[1]),
+                                        y=slice(extent[2], extent[3]))
+regis_ds_raw = regis_ds_raw[['top', 'bottom', 'kD', 'c', 'kh', 'kv']]
+
+nlay, lay_sel = mgrid.get_number_of_layers_from_regis(regis_ds_raw)
+regis_ds_raw = regis_ds_raw.sel(layer=lay_sel)
+
+# %% write netcdf
+datadir = '../data'
+regis_ds_raw.to_netcdf(os.path.join(datadir, 'regis_ugw_test.nc'))
